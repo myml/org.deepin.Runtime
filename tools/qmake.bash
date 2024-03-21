fd1243fc96bed15f077b28c3f06fc1e1d19e07e6
myml <wurongjie@deepin.org>
2024-03-21 13:53:36 +0800

    fix: qt5integration install failed

--- tools/qmake.bash
+++ tools/qmake.bash
@@ -7,7 +7,8 @@ while IFS= read -r pkg; do
     cd "$project"
     mkdir -p "linglong/sources/.build_cache/$pkg"
     cd "linglong/sources/.build_cache/$pkg"
-    qmake -makefile PREFIX="${PREFIX}" LIB_INSTALL_DIR="${PREFIX}/lib/${TRIPLET}" "../../$pkg"
+    # MIME_TYPE_FILE_INSTALL_PATH 是 qt5integration 需要用到的
+    qmake -makefile PREFIX="${PREFIX}" MIME_TYPE_FILE_INSTALL_PATH="$PREFIX/share/mime/packages" LIB_INSTALL_DIR="${PREFIX}/lib/${TRIPLET}" "../../$pkg"
     make -j8
     make install
 done;
