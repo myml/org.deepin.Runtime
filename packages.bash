dafb37587bb12293ac8ad130554f20cbe80b5a61
myml <wurongjie@deepin.org>
2024-10-22 14:50:28 +0800

    feat: using deb to create runtime
    使用deb制作runtime, 以便于和系统共用二进制文件

--- packages.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-grep ^Package: "linglong/sources/qtbase-opensource-src/debian/control"
-./modules.list.sh | while IFS= read -r pkg 
-do
-    grep ^Package: "linglong/sources/$pkg/debian/control"
-done
